7a9b191126705baa9f398f0a601ea10ce01ce983
ArnaudG0649 <arnaudcmc@hotmail.com>
2024-04-17 18:29:20 +0200

    a

--- Projet_BDDR/projetenron/test/testreq.sql
+++ Projet_BDDR/projetenron/test/testreq.sql
@@ -131,3 +131,52 @@ SELECT Ti.lastname, Ti.firstname, (Ti.nbinterne + Te.nbexterne) as nbmail, Ti.nb
         ON Ti.employee_id=Te.employee_id 
     WHERE Ti.nbinterne + Te.nbexterne>100
 ;
+
+----Requête 4----
+
+SELECT emp.employee_id, emp2.employee_id, COUNT(m.mail_id) as nbmail /*Cette requête compte le nombre de mailS que l'employé de gauche à envoyé à l'employé de droite*/
+    FROM app1_employee emp
+    INNER JOIN app1_emailadress ea ON emp.employee_id=ea.employee_id_id
+    LEFT JOIN app1_mail m ON m.emailadress_id_id=ea.emailadress_id
+    INNER JOIN app1_to t ON t.mail_id_id=m.mail_id
+    LEFT JOIN app1_emailadress ea2 ON ea2.emailadress_id=t.emailadress_id_id
+    INNER JOIN app1_employee emp2 ON emp2.employee_id=ea2.employee_id_id
+GROUP BY emp.employee_id, emp2.employee_id
+ORDER BY emp.employee_id, emp2.employee_id
+;
+
+
+
+(SELECT t.employee_id, COUNT(m.mail_id), m.emailadress_id_id FROM app1_mail m 
+RIGHT JOIN
+    (SELECT ea2.employee_id, t.mail_id_id FROM app1_to t 
+    RIGHT JOIN
+        (SELECT ea2.emailadress_id, emp2.employee_id FROM app1_emailadress ea2
+                    INNER JOIN app1_employee emp2 ON emp2.employee_id=ea2.employee_id_id) ea2 ON ea2.emailadress_id=t.emailadress_id_id /*les adresses mails des destinataires*/
+    ) t ON t.mail_id_id=m.mail_id
+GROUP BY t.employee_id, m.emailadress_id_id
+) m 
+
+
+
+
+
+
+SELECT m.employee_id, m.nbmail, m.emailadress_id_id, ea.employee_id FROM 
+(SELECT ea.emailadress_id, emp.employee_id FROM app1_emailadress ea
+                INNER JOIN app1_employee emp ON emp.employee_id=ea.employee_id_id
+) ea
+RIGHT JOIN
+(SELECT t.employee_id, COUNT(m.mail_id) AS nbmail, m.emailadress_id_id FROM app1_mail m 
+RIGHT JOIN
+    (SELECT ea2.employee_id, t.mail_id_id FROM app1_to t 
+    RIGHT JOIN
+        (SELECT ea2.emailadress_id, emp2.employee_id FROM app1_emailadress ea2
+                    INNER JOIN app1_employee emp2 ON emp2.employee_id=ea2.employee_id_id) ea2 ON ea2.emailadress_id=t.emailadress_id_id /*les adresses mails des destinataires*/
+    ) t ON t.mail_id_id=m.mail_id
+GROUP BY t.employee_id, m.emailadress_id_id
+ORDER BY t.employee_id
+) m ON ea.emailadress_id=m.emailadress_id_id
+ORDER BY m.employee_id
+;
+
